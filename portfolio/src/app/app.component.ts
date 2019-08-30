151e0b03a5a4b705aa30bbf2cda3025e7c780e8c
agtrevino <39744373+agtrevino@users.noreply.github.com>
2019-08-29 19:07:20 -0500

    updates

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import $ from "jquery";
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'portfolio';
 }
+
+$(window).scroll(function(){
+  var scroll = $(window).scrollTop();
+  if(scroll < 100){
+      $('.navbar').css('background', 'transparent');
+      $('.navbar').css('transition', '400ms');
+      $('.navbar').css('height', '80px');
+      $('.nav-item').css('margin', ' 0px 15px');
+      $('.nav-item').css('transition', '400ms');
+  } else{
+      $('.navbar').css('background', 'rgba(0, 0, 0, 0.3)');
+      $('.navbar').css('transition', '400ms');
+      $('.navbar').css('height', '60px');
+      $('.nav-item').css('margin', ' 0px 30px');
+      $('.nav-item').css('transition', '400ms');
+  }
+});
