c4ba1f82ce30620c4b739885ef9002ceca8549c6
Aaron Trevino <agtrevino91@yahoo.com>
2020-02-04 16:06:29 -0600

    updates made to things

--- portfolio/src/app/home-two/home-two.component.ts
+++ portfolio/src/app/home-two/home-two.component.ts
@@ -20,7 +20,6 @@ export class HomeTwoComponent implements OnInit {
     this.resumeTitleViewCheck();
   }
 
-
   closeSubNav() {
     $('.sub-menu-container').removeClass("expanded")
     setTimeout(function () {
